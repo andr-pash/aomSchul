84da580839105c6b720e85834f4ed89c02300fa3
Andreas Pashalides <pashalides@apiomat.com>
2017-07-06 12:29:26 +0200

    added profile component

---
+++ src/app/client-profile-page/client-profile-page.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const Apiomat
+
+@Component({
+  selector: 'app-client-profile-page',
+  templateUrl: './client-profile-page.component.html',
+  styleUrls: ['./client-profile-page.component.css']
+})
+export class ClientProfilePageComponent implements OnInit {
+  public client = new Apiomat.Client()
+
+  constructor() { }
+
+  ngOnInit() {
+    const client = new Apiomat.Client("Kristof", "secret")
+    client.loadMe({
+      onOk: () => {
+        client.loadInsuredBy({
+          onOk: () => this.client = client,
+          onError: (err) => console.error(err)
+        })
+      },
+      onError: (err) => console.error(err)
+    })
+
+
+  }
+
+}
